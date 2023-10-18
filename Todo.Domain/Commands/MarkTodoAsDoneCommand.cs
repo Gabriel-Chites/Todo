3990d81ed19a0acba8fd540ae88d056d3c6e8647
Gabriel Chites Alves <gabriel.chites@vortigo.com.br>
2023-10-18 16:55:29 -0300

    Commands finished

---
+++ Todo.Domain/Commands/MarkTodoAsDoneCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Domain.Commands;
+
+public class MarkTodoAsDoneCommand : MarkTodoCommand
+{
+    public MarkTodoAsDoneCommand() : base() { } 
+    
+    public MarkTodoAsDoneCommand(Guid id, string user) 
+        : base(id, user) { }
+}
---
+++ Todo.Domain/Commands/MarkTodoAsUndoneCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Domain.Commands;
+
+public class MarkTodoAsUndoneCommand : MarkTodoCommand
+{
+    public MarkTodoAsUndoneCommand() : base() { }
+
+    public MarkTodoAsUndoneCommand(Guid id, string user) 
+        : base(id, user) { }
+}
